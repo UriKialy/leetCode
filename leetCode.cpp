15e9675680cc8647212539aac54f7f0b7f6b9db4
UriKialy <sheffi1414@gmail.com>
2024-11-20 12:54:46 +0200

    1st upload

---
+++ leetCode.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+
+ //Definition for a binary tree node.
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+ 
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root == nullptr) return 0;
+        int left = maxDepth(root->left);
+        int right = maxDepth(root->right);
+        return std::max(left, right) + 1;
+        
+    }
+};
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
